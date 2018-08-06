f80dd27920c357978d81a9e2b7c0396cc58d18df
kolintsio96 <kolintsiovolodymyr@gmail.com>
2018-08-06 13:08:26 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,77 +1,13 @@
-import 'fullpage.js/dist/fullpage.min.css';
-import 'fullpage.js/vendors/easings.min';
-import 'fullpage.js/vendors/scrolloverflow.min';
-import fullpage from 'fullpage.js/dist/fullpage.min';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import slick from 'slick-carousel/slick/slick.min';
-import $ from 'jquery';
-
-new fullpage('#fullpage', {
-    ///key///
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    ///navigation///
-    menu: '#menu',
-    anchors: ['hero', 'services', 'projects', 'advantages', 'about', 'contacts'],
-    onLeave: (origin, destination, direction) => {
-        if (destination.anchor === 'advantages') {
-            let currentNumber = $('.advantages__block .number');
-            [...currentNumber].forEach((el) => {
-                let number = parseInt($(el).text().replace(' ', ''));
-                 console.log(number);
-                $({numberValue: 0}).animate({numberValue: number}, {
-                    duration: 2000,
-                    easing: 'linear',
-                    step: function () {
-                        $(el).text(Math.ceil(this.numberValue));
-                    }
-                });
-            });
-
-        }
-    },
+let form = document.querySelector('.subscribe');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    axios.post('/send_email/', {
+        email: form.querySelector('input').value,
+    })
+        .then(function (response) {
+            console.log(form.querySelector('input').value);
+        })
+        .catch(function (error) {
+            console.log(form.querySelector('input').value);
+        });
 });
-
-///slider///
-let projects = document.querySelector('.projects');
-$('.bg-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: false,
-    asNavFor: '.main-slider'
-});
-$('.main-slider').slick({
-    arrow: true,
-    centerMode: true,
-    centerPadding: '320px',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.bg-slider'
-});
-$('.slider-img').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
-    infinite: true,
-    asNavFor: '.slider-word'
-});
-$('.slider-word').slick({
-    arrow: false,
-    centerMode: true,
-    vertical:true,
-    verticalSwiping:true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-img'
-});
-
-
-
